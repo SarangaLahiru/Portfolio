f8f8394312a0bd430fd284eed1dd52c227cde080
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-08-17 20:21:10 +0530

    update_2

--- vite-project/src/Components/HomeComponent/About/About.jsx
+++ vite-project/src/Components/HomeComponent/About/About.jsx
@@ -1,100 +1,125 @@
-import { VerifiedUserSharp } from '@mui/icons-material'
-import { Grid, Table, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
-import React from 'react'
+import { VerifiedUserSharp } from '@mui/icons-material';
+import { Grid, Table, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
+import React from 'react';
 
 export default function About() {
     return (
-        <div>
+        <div className='p-4'>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={5}>
-                    <div className=''>
-                        <img src="./Images/man-12326.png" alt="" />
+                    <div className='flex justify-center'>
+                        <img src="./Images/man-12326.png" alt="Profile" className='w-full h-auto rounded-lg shadow-lg' />
                     </div>
                 </Grid>
                 <Grid item xs={12} sm={7}>
-                    <div className=' p-[90px]'>
-
+                    <div className='p-[20px]'>
                         <Typography
                             variant="body1" color="initial"
                             sx={{
                                 color: "#FF9843",
                                 fontSize: "30px",
                                 fontFamily: '"Poppins", sans-serif',
+                                marginBottom: "10px"
                             }}
                         >
-                            He Intro
+                            Introduction
                         </Typography>
                         <Typography
                             variant="body1" color="initial"
                             sx={{
-
                                 fontSize: "50px",
                                 fontFamily: '"Poppins", sans-serif',
-                                fontWeight: "700"
+                                fontWeight: "700",
+                                marginBottom: "20px"
                             }}
                         >
-                            About Him
+                            About Me
                         </Typography>
                         <Typography
                             variant="body1" color="initial"
                             sx={{
-
                                 fontSize: "20px",
                                 fontFamily: '"Poppins", sans-serif',
-                                fontWeight: "200",
-                                color: "#8c8888"
+                                fontWeight: "300",
+                                color: "#8c8888",
+                                lineHeight: "1.6"
                             }}
                         >
-                            A wiki is a form of online hypertext publication that is collaboratively edited and managed by its audience directly through a web browser. A typical wiki contains multiple pages that can either be edited by the public or limited to use within an organization for maintaining its internal knowledge base
+                            I am a Full-Stack Developer with expertise in both frontend and backend technologies. I enjoy building dynamic and responsive web applications and have experience working with modern frameworks and technologies such as React, Node.js, and Laravel. My passion is to create engaging user experiences and solve complex problems through code.
                         </Typography>
                         <TableContainer
                             sx={{
-                                width: "200px",
-                                marginTop: "40px"
-                            }}>
+                                marginTop: "40px",
+                                border: '1px solid #e0e0e0',
+                                borderRadius: '8px'
+                            }}
+                        >
                             <Table>
-                                <TableRow
-
-                                >
-                                    <TableCell sx={{
-                                        border: "none"
-                                    }}
+                                <TableRow>
+                                    <TableCell
+                                        sx={{
+                                            border: "none",
+                                            fontWeight: "600",
+                                            padding: '10px'
+                                        }}
                                     >
-                                        <VerifiedUserSharp />
-                                        Name :
-
+                                        <VerifiedUserSharp sx={{ color: "#FF9843", marginRight: "8px" }} />
+                                        Name:
                                     </TableCell>
                                     <TableCell
                                         sx={{
-                                            border: "none"
+                                            border: "none",
+                                            padding: '10px'
                                         }}
-                                    >sff</TableCell>
-                                </TableRow>
-                                <TableRow
-
-                                >
-                                    <TableCell sx={{
-                                        border: "none"
-                                    }}
                                     >
-                                        <VerifiedUserSharp />
-                                        Email :
-
+                                        Sashika Lahiru
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell
+                                        sx={{
+                                            border: "none",
+                                            fontWeight: "600",
+                                            padding: '10px'
+                                        }}
+                                    >
+                                        <VerifiedUserSharp sx={{ color: "#FF9843", marginRight: "8px" }} />
+                                        Email:
                                     </TableCell>
                                     <TableCell
                                         sx={{
-                                            border: "none"
+                                            border: "none",
+                                            padding: '10px'
                                         }}
-                                    >sff</TableCell>
+                                    >
+                                        sashika.lahiru@example.com
+                                    </TableCell>
                                 </TableRow>
-
+                                <TableRow>
+                                    <TableCell
+                                        sx={{
+                                            border: "none",
+                                            fontWeight: "600",
+                                            padding: '10px'
+                                        }}
+                                    >
+                                        <VerifiedUserSharp sx={{ color: "#FF9843", marginRight: "8px" }} />
+                                        Skills:
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            border: "none",
+                                            padding: '10px'
+                                        }}
+                                    >
+                                        Full-Stack Development, React, Node.js, Laravel
+                                    </TableCell>
+                                </TableRow>
                             </Table>
                         </TableContainer>
                     </div>
                 </Grid>
-
             </Grid>
-
         </div>
-    )
+    );
 }
