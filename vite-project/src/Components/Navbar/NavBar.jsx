978ca00b93c540d034b976b8f65e0ae85ed4cd33
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-08-18 19:53:47 +0530

    update

--- vite-project/src/Components/Navbar/NavBar.jsx
+++ vite-project/src/Components/Navbar/NavBar.jsx
@@ -115,7 +115,7 @@ function NavBar() {
                 fontSize: "50px",
               }}
             >
-              LOGO
+              LOG
             </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
